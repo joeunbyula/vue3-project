bab59fdee2d9ab078f98a40b9aaa4cfab6275286
eunbyul <eunbyul@eunbyului-MacBookPro.local>
2026-02-03 15:33:58 +0900

    함수 쓰기

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,38 @@
 <template>
-  <div>eunbyul</div>
+  <div class="name">
+    {{ greet }}
+  </div>
+
+  <!-- <div class="name">
+    //함수쓰는법!
+    {{ greeting(name) }}
+  </div> -->
+
 </template>
+
+<script>
+  export default {
+    setup() {
+      const name = 'eunbyul is me!';
+
+      const greeting = (name) => {
+        return 'Hello! ' + name;
+      };
+
+      const greet = greeting(name);
+
+      return {
+        name,
+        greet,
+        greeting
+      }
+    }
+  }
+</script>
+
+<style>
+  .name {
+    color: slateblue;
+  }
+</style>
 
